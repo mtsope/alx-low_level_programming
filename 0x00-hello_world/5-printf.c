35a98b7efa9ead84152968e7f6b042d124c1c123
Michela Tsope <michelatsope4@gmail.com>
2022-12-11 10:13:51 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * main - Ponto de entrada
- *description: 'print Programming is like building a multilingual puzzle'
+ * description: 'print Programming is like building a multilingual puzzle'
  * Return: 0
-*/
+ */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
