1fcd3732f397e03b029e20179acbd3dee79d9324
Michela Tsope <michelatsope4@gmail.com>
2022-12-11 09:38:59 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Ponto de entrada
+ *description: 'print Programming is like building a multilingual puzzle'
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
