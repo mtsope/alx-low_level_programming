a830468e888a7a9eb37c8f573dc1f999c5fd43b5
Michela Tsope <michelatsope4@gmail.com>
2022-12-12 05:29:09 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Ponto de entrada
+ * description: 'This program prints the alphabet in lowercase.'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number < 10)
+	{
+		printf("%i", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
