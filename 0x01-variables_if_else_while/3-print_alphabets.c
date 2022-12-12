f680b5aa6c91a62fed2a7b17ebba77e1b13f9cbe
Michela Tsope <michelatsope4@gmail.com>
2022-12-12 04:46:38 -0800

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Ponto de entrada
+ * description: 'This program prints the alphabet in lowercase.'
+ * Return: 0
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+	putchar('\n');
+	return (0);
+}
