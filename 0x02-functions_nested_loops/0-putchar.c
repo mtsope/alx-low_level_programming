d932c641ac67d91baebcd9f0ac458ea2d17deab7
Michela Tsope <michelatsope4@gmail.com>
2022-12-13 12:58:28 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Ponto de entrada
+ * description: 'Write a program that prints _putchar'
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
